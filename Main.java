e107cf6bcd17a29dbca5a519822b031eb2efb3c7
Girlc7442 <girlc7442@wrdsb.ca>
2020-09-15 13:54:40 +0000

    Finished with comments

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *Determines if a number is even or odd by using remainder
+ * @author Cole G.
  */
 public class Main {
 
@@ -9,7 +11,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Ask the user for a number
+    System.out.println("Please enter an integer:");
+    //Get the number from the user
+    int userNumber  = input.nextInt();
     
+    //The equation for determining if a number is even or odd
+    int remainder = userNumber % 2;
+
+    //Check to see if the number is even or odd
+    if(remainder >= 1){
+      //tells them the number is odd
+      System.out.println(userNumber + " is an odd number");
+    }else{
+      //tells them the number is even
+      System.out.println(userNumber + " is an even number");
+    }
   }
 }
